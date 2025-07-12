a89b5b5b7797ebbe9c17b15d4c25e184e8802924
phoebe lartisant <phoebe.contactme@gmail.com>
2025-07-12 10:50:54 -0400

    Update Website

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple, serious interactions only
-document.addEventListener('DOMContentLoaded', function() {
-    // No animations - direct display
-    console.log('France Chômage - Site loaded');
-});
