2c875a7414418c983f117e0b34b4bb5b1f62e8cd
phoebe <phoebe.contactme@protonmail.com>
2025-07-12 02:51:01 -0400

    init website

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple, serious interactions only
+document.addEventListener('DOMContentLoaded', function() {
+    // No animations - direct display
+    console.log('France Chômage - Site loaded');
+});
